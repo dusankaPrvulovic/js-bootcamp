f4dd4bb251aebc3643194a24c524c4857938473c
Dusanka Prvulovic <38867348+dusankaPrvulovic@users.noreply.github.com>
2019-02-04 14:38:53 +0100

    Add files via upload

---
+++ l1-fizzbuzz.js
@@ -0,0 +1,9 @@
+for (let number = 0; number <=100; number = number + 1)
+	if (number == 3) {
+      console.log("Fizz");
+    } else if (number %5) {
+      console.log(number);
+    } else {
+      console.log("buzz");
+    }
+console.log(number)
