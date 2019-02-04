fff0784b56020f2b2b9ec7bd91714cca0dff76af
Dusanka Prvulovic <38867348+dusankaPrvulovic@users.noreply.github.com>
2019-02-04 14:37:21 +0100

    Add files via upload

---
+++ l1-looping-a-triangle.js
@@ -0,0 +1,7 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+for (let triangles = '#'; triangles.length <= 8; triangles.length = triangles += '#') {
+    console.log(triangles);
+}
